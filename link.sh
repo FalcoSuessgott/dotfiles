a7c04ac1cafc294097f23dbe830da14ae01577f6
Tom Morelly <tom-morelly@gmx.de>
2020-04-10 15:24:05 +0200

    refactored

--- link.sh
+++ link.sh
@@ -9,37 +9,20 @@
 #
 ###################################################
 
-SRC="dotfiles"
-ZSH_CUSTOM=".oh-my-zsh/custom"
-
-function link(){
-    # $1 src
-    # $2 dest
-    ln -sfv ~/$SRC/$1 ~/$2
-}
-
 function main(){
-  # Oh my zsh
-  # sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-
-  # Powerlevel10k
-  # git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-    
-  # dotfiles
-  ln -sfv ~/dotfiles/dotfiles/zshrc ~/.zshrc
   ln -sfv ~/dotfiles/dotfiles/vimrc ~/.vimrc
   ln -sfv ~/dotfiles/dotfiles/profile ~/.profile
   ln -sfv ~/dotfiles/dotfiles/bashrc ~/.bashrc
   ln -sfv ~/dotfiles/dotfiles/bash_aliases ~/.bash_aliases
   ln -sfv ~/dotfiles/dotfiles/gitconfig ~/.gitconfig
-
-  # zsh plugins
   ln -sfv ~/dotfiles/zsh_custom_plugins/git ~/.oh-my-zsh/custom/plugins/.
   ln -sfv ~/dotfiles/zsh_custom_plugins/git-flow-completion ~/.oh-my-zsh/custom/plugins/.
-
-  # configs
   ln -sfv ~/dotfiles/config/bspwmrc ~/.config/bspwm/.
   ln -sfv ~/dotfiles/config/sxhkd/sxhkdrc ~/.config/sxhkdrc/.
+  ln -sfv ~/dotfiles/config/polybar/config ~/.config/polybar/.
+  ln -sfv ~/dotfiles/config/polybar/launch.sh ~/.config/polybar/.
+  ln -sfv ~/dotfiles/config/.xinitrc ~/.
+  ln -sfv ~/dotfiles/config/Xresources ~/.
 }
 
 main 
