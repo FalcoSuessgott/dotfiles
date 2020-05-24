36890f3e4a70a2e00cbce2568195adc376286899
Tom Morelly <tom-morelly@gmx.de>
2020-05-24 20:51:23 +0200

    removed vars

--- scripts/connect_screens.sh
+++ scripts/connect_screens.sh
@@ -5,6 +5,9 @@
 ###########################################################
 
 
-xrandr --output DVI-I-2-2 --auto --right-of eDP-1 --output DVI-I-1-1 --auto --right-of DVI-I-2-2
+[ "$1" == "home" ] &&  xrandr --output DVI-I-2-2 --auto --right-of eDP-1 --output DVI-I-1-1 --auto --right-of DVI-I-2-2
+[ "$1" == "sva" ] && xrandr --output HDMI-1 --auto --right-of eDP-1
+
+
 bspc wm -r 
 
