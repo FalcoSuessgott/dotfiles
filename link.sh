f65cdf831de5c0fe7ef86a9b8f8adf4f8e7e5284
Tom Morelly <tom-morelly@gmx.de>
2020-09-07 10:23:27 +0200

    introducing tdrop as scratchpad

--- link.sh
+++ link.sh
@@ -19,31 +19,20 @@ function main(){
     # /config 
     link config/bash/bashrc .bashrc
     link config/bash/bash_aliases .bash_aliases
-
     link config/bspwm/bspwmrc .config/bspwm/.
-
     link config/git/gitconfig .gitconfig
-
     link config/lf/lfrc .config/lf/.
-
     link config/polybar/ .config/polybar/.
-
     link config/profile .profile
-
     link config/sxhkd/sxhkdrc .config/sxhkd/.
-
     link config/vim/vimrc .vimrc
-
     link config/Xresources .Xresources
-
     link config/zsh/zshrc .zshrc
     link config/zsh/zsh_custom_plugins/git .oh-my-zsh/custom/plugins/.
     link config/zsh/zsh_custom_plugins/git-flow-completion .oh-my-zsh/custom/plugins/.
 
     # /scripts 
-    link scripts/pash .local/bin/.
-
-    link scripts/connect_screens.sh .local/bin/,
+    link scripts/tdrop  .local/bin/.
 }
 
 main 
--- scripts/connect_screens.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Joins external monitors and restarts polybar and bspwm
-#
-###########################################################
-
-
-[ "$1" == "home" ]  && xrandr --output DVI-I-2-2 --auto --right-of eDP-1 --output DVI-I-1-1 --auto --right-of DVI-I-2-2
-[ "$1" == "sva" ]   && xrandr --output DP-2-2 --auto --right-of DP-2-1 --output eDP-1 --auto --right-of DP-2-2
