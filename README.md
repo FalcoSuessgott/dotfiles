22a1d0c494782a931203b1af83f67e7233dd4b33
Tom Morelly <tom-morelly@gmx.de>
2022-10-11 21:29:18 +0200

    refactor

--- README.md
+++ README.md
@@ -1,26 +1,11 @@
-# My Dotfiles for Ubuntu (18.04, 19.04, 20.04)
+# Packages
+* bspwm
+* sxhkd
+* polybar
+* zsh / oh-my-zsh
+* alacritty
+* spaceship
+* xcompmgr
+* direnv
+* vscodium
 
-![img](dotfiles.png)
-
-# Overview
-| Topic           | Tool      |
-|-----------------|-----------|
-| Terminal        | alacritty |
-| Windowmanager   | bspwm     |
-| Shortcutmanager | sxhkdrc   |
-| Statusbar       | polybar   |
-| Filemanager     | lf        |
-| Shell           | zsh       |
-| Editor          | Vim       |
-
-## Features
-* change wallpaper and adjust the terminals acoording its colors
-* ...
-
-```
-
-
-## Link dotfiles
-```
-bash link.sh
-```
--- link.sh
+++ link.sh
@@ -8,35 +8,56 @@
 #   12032020
 #
 ###################################################
-SRC="$HOME/git/github/dotfiles"
+SRC="$HOME/git/dotfiles"
 
 function link(){
     ln -sfv $SRC/$1 ~/$2
 }
 
 function main(){
+	
+	# alacritty
+	link config/alacritty/alacritty.yml .config/alacritty/.
+	link scripts/spawn-alacritty-cwd .local/bin/.
 
-    # /config 
-    link config/alacritty/alacritty.yml .config/alacritty/.
-    link config/bash/bashrc .bashrc
-    link config/bash/bash_aliases .bash_aliases
-    link config/bash/bash_profile .bash_profile
-    link config/bspwm/bspwmrc .config/bspwm/.
-    link config/git/gitconfig .gitconfig
-    link config/flashfocus/flashfocus.yml .config/flashfocus/.
-    link config/lf/lfrc .config/lf/.
-    link config/polybar/ .config/.
-    link config/profile .profile
-    link config/sxhkd/sxhkdrc .config/sxhkd/.
-    link config/vim/vimrc .vimrc
-    link config/Xresources .Xresources
-    link config/zsh/zshrc .zshrc
-    link config/zsh/zsh_custom_plugins/git .oh-my-zsh/custom/plugins/.
-    link config/zsh/zsh_custom_plugins/git-flow-completion .oh-my-zsh/custom/plugins/.
+	# bash
+	link config/bash/bashrc .bashrc
+	link config/bash/bash_aliases .bash_aliases
+	link config/bash/bash_profile .bash_profile
 
-    # /scripts 
-    link scripts/tdrop  .local/bin/.
-    link scripts/spawn-alacritty-cwd .local/bin/.
+	# bspwm
+	link config/bspwm/bspwmrc .config/bspwm/.
+	
+	# ulauncher
+	link config/ulauncher/user-themes/ReMatch-Dark-Blue-Ulauncher .config/ulauncher/user-themes/.
+
+	# mkcert
+	link config/mkcert .mkcert
+	# git
+    	link config/git/gitconfig .gitconfig
+
+    	# polybar	
+	link config/polybar/ .config/.
+
+	# link config/profile .profile
+
+	# sxhkd
+	link config/sxhkd/sxhkdrc .config/sxhkd/.
+
+	# link config/vim/vimrc .vimrc
+	# link config/Xresources .Xresources
+
+	# terraform
+	link config/terraform/.tfenv .
+
+	# golang
+	link config/go/.gvm .
+
+	# zsh
+	link config/zsh/zshrc .zshrc
+	link config/zsh/zsh_custom_plugins/git .oh-my-zsh/custom/plugins/.
+	link config/zsh/zsh_custom_plugins/git-flow-completion .oh-my-zsh/custom/plugins/.
+	link config/zsh/zsh_custom_plugins/zsh-autosuggestions .oh-my-zsh/custom/plugins/.
 }
 
 main 
